113857a74580f8d56e568ff3985977933fa3d61d
Vaibhav221207 <sejuvaibhav.in.com@gmail.com>
2026-07-15 11:03:15 +0530

    Make app animation-only

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask, render_template, jsonify
-import json
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-DATA_PATH = os.path.join('data', 'sample_data.json')
-
-
-def load_data():
-    with open(DATA_PATH, 'r', encoding='utf-8') as f:
-        return json.load(f)
 
 
 @app.route('/')
 def index():
-    data = load_data()
-    return render_template('index.html', data=data)
-
-
-@app.route('/api/data')
-def api_data():
-    return jsonify(load_data())
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
